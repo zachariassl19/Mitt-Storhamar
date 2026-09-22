430f66dd05c7d127dd5148b0eb099eaf6ef26d85
zachariassl19 <zacharias09solberg@gmail.com>
2026-09-22 12:21:15 +0200

    style: load game detail styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './AppV2'
 import './styles.css'
 import './logoStyles.css'
+import './detailStyles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
