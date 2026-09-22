db951e2f003b6460a61b406024aa82a3f98031e7
zachariassl19 <zacharias09solberg@gmail.com>
2026-09-22 10:48:31 +0200

    feat: add initial 2026-27 schedule data

---
+++ src/data/games.ts
@@ -0,0 +1,49 @@
+import type { Game } from '../types'
+
+export const games: Game[] = [
+  {
+    id: 'game:2026-09-24-ringerike',
+    startsAt: '2026-09-24T18:30:00+02:00',
+    competition: 'EHL',
+    homeTeam: 'Storhamar',
+    awayTeam: 'Ringerike',
+    arena: 'CC Amfi',
+    city: 'Hamar',
+  },
+  {
+    id: 'game:2026-09-26-stavanger',
+    startsAt: '2026-09-26T16:00:00+02:00',
+    competition: 'EHL',
+    homeTeam: 'Stavanger',
+    awayTeam: 'Storhamar',
+    arena: 'DNB Arena',
+    city: 'Stavanger',
+  },
+  {
+    id: 'game:2026-10-01-nidaros',
+    startsAt: '2026-10-01T18:30:00+02:00',
+    competition: 'EHL',
+    homeTeam: 'Nidaros',
+    awayTeam: 'Storhamar',
+    arena: 'Leangen Arena',
+    city: 'Trondheim',
+  },
+  {
+    id: 'game:2026-10-03-valerenga',
+    startsAt: '2026-10-03T16:00:00+02:00',
+    competition: 'EHL',
+    homeTeam: 'Storhamar',
+    awayTeam: 'Vålerenga',
+    arena: 'CC Amfi',
+    city: 'Hamar',
+  },
+  {
+    id: 'game:2026-10-07-tappara',
+    startsAt: '2026-10-07T17:30:00+03:00',
+    competition: 'CHL',
+    homeTeam: 'Tappara',
+    awayTeam: 'Storhamar',
+    arena: 'Nokia Areena',
+    city: 'Tampere',
+  },
+]
