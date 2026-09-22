a4ada88bda3bce21a2295e4cc65048eaa47ba77e
zachariassl19 <zacharias09solberg@gmail.com>
2026-09-22 12:04:04 +0200

    feat: activate full season and logo UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './AppV2'
 import './styles.css'
+import './logoStyles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
