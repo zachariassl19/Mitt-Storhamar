a63dff6a098099ecb5ce8cc35ab23f379c997f87
zachariassl19 <zacharias09solberg@gmail.com>
2026-09-23 23:39:22 +0200

    feat: mount dynamic matchday home

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './AppV2'
 import { CloudSyncManager } from './components/CloudSyncManager'
+import { DynamicMatchdayHome } from './components/DynamicMatchdayHome'
 import { NotificationManager, NotificationSettingsPortal } from './components/NotificationCenter'
 import { SmartGameDayManager, SmartGameDaySettingsPortal } from './components/SmartGameDayGlobal'
 import './styles.css'
@@ -12,6 +13,7 @@ import './unifiedTravel.css'
 import './gps.css'
 import './smartGameDaySettings.css'
 import './notifications.css'
+import './dynamicHome.css'
 import './purchases.css'
 import './cloudSync.css'
 import './gameCards.css'
@@ -20,6 +22,7 @@ import './minStorhamar.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <DynamicMatchdayHome />
     <SmartGameDayManager />
     <SmartGameDaySettingsPortal />
     <NotificationManager />
